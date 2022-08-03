274a94a1f5ae1b43784d3b4eb5ab6a733e687b41
Lucas Machado Jofre <mjofrelucas@gmail.com>
2022-08-03 02:36:05 -0300

    Added getter and setter through property decorator in Block class. Removed draw_grid() method from Board class. Fixed get_alive_neighbours to calculate the neighbours on aux_matrix instead of matrix. Added removed methods as function of main.py.

--- block.py
+++ block.py
@@ -4,14 +4,15 @@ import settings as s
 class Block:
     def __init__(self, state: int) -> None:
         self.state = state
-        self._color = s.BLACK
+        self.color = s.BLACK
 
-    # todo: check if i can remove screen drawing as part of this class
-    def draw_block(self, position: tuple) -> None:
-        self._change_color()
-        pygame.draw.rect(screen, self._color, pygame.Rect(position[0] * BLOCK_SIZE + GRID_WIDTH,
-                                                          position[1] * BLOCK_SIZE + GRID_WIDTH,
-                                                          BLOCK_SIZE - GRID_WIDTH, BLOCK_SIZE - GRID_WIDTH))
+    @property
+    def state(self) -> int:
+        return self._state
 
-    def _change_color(self):
-        self._color = s.BLACK if self.state == 0 else s.WHITE
+    @state.setter
+    def state(self, value: int) -> None:
+        if value not in [0, 1]:
+            raise ValueError("State should be either 0 or 1")
+        self._state = value
+        self.color = s.BLACK if self.state == 0 else s.WHITE
--- board.py
+++ board.py
@@ -19,19 +19,12 @@ class Board:
         return [[Block(state=random.randint(0, 1)) for _ in range(self.x_dim)] for _ in
                 range(self.y_dim)]
 
-    # todo: check if i can remove screen drawing as part of this class. staticmethod code smell
-    @staticmethod
-    def draw_grid() -> None:
-        for i in range(0, s.WINDOWS_WIDTH, s.BLOCK_SIZE):
-            pygame.draw.line(screen, GREY, (0, i), (WINDOWS_WIDTH, i), GRID_WIDTH)
-            pygame.draw.line(screen, GREY, (i, 0), (i, WINDOWS_HEIGHT), GRID_WIDTH)
-
     def get_alive_neighbours(self, x: int, y: int) -> int:
         total = 0
         for y_neighbour in range(y - 1, y + 2):
             for x_neighbour in range(x - 1, x + 2):
                 try:
-                    total += self.matrix[y_neighbour][x_neighbour].state
+                    total += self.aux_matrix[y_neighbour][x_neighbour].state
                 except IndexError as e:
                     continue
                 # if ((x_neighbour, y_neighbour) == (x, y) or
@@ -40,5 +33,5 @@ class Board:
                 #         x_neighbour == 0 or
                 #         y_neighbour == 0):
                 #     continue
-        total -= self.matrix[y][x].state
+        total -= self.aux_matrix[y][x].state
         return total
